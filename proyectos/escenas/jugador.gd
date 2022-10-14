9d6043e25bd19493829a2ba08a383efab0007e12
bossaofficial <114254678+bossaofficial@users.noreply.github.com>
2022-10-14 12:19:18 -0300

    AAAAAAAAAAAAAAA

--- proyectos/escenas/jugador.gd
+++ proyectos/escenas/jugador.gd
@@ -7,6 +7,7 @@ var velocity = Vector2()
 var GRAVITY = 100
 var SPEED = 600
 var SPEEDY = 600
+var KICK_IMPULSE = 10
 export (int, 0, 200) var push = 100
 
 export (int) var input_index = 1
@@ -16,7 +17,7 @@ func _physics_process(delta):
 	var move_input1 = Input.get_axis("move_up" + str(input_index),"move_down" + str(input_index))
 	velocity.x = move_input * SPEED
 	velocity.y = move_input1 * SPEEDY
-	velocity = move_and_slide(velocity,Vector2.ZERO, false, 4, PI/4,true)
+	velocity = move_and_slide(velocity,Vector2.ZERO, false, 4, PI/4,false)
 	for index in get_slide_count():
 		var collision = get_slide_collision(index)
 		if collision.collider.is_in_group("bodies"):
@@ -24,7 +25,13 @@ func _physics_process(delta):
 
 	if move_input > 0 or move_input1 > 0:
 		anim_player.play("idle")
-
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		var ball:= collision.collider as Ball
+		if ball:
+			ball.apply_central_impulse((-collision.normal + Vector2.UP*0.5)*velocity.length()*KICK_IMPULSE/1000)
+		
+			
 # Animaciones
 
 
